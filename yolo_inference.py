f7a982274d4e396e157aeac3eccd49619d5d792b
Eslam Reda <154593563+EslamReda11@users.noreply.github.com>
2024-04-18 06:37:49 +0200

    Update yolo_inference.py

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,9 +1,33 @@
 from ultralytics import YOLO 
-
 model = YOLO('yolov8x')
 
-result = model.track('input_videos/input_video.mp4',conf=0.2, save=True)
+
+# predict 1
+# model.predict('input_videos/image.png',save=True)
+
+# predict 2
+# result=model.predict('input_videos/image.png',save=True)
 # print(result)
 # print("boxes:")
 # for box in result[0].boxes:
-#     print(box)
+#   print(box)
+
+# predict 3
+# result=model.predict('input_videos/input_video.mp4',save=True)
+# print(result)
+# print("boxes:")
+# for box in result[0].boxes:
+#   print(box)
+  
+# predict 4
+# model = YOLO('models/yolo5_last.pt')
+# result=model.predict('input_videos/input_video.mp4',conf=0.2,save=True)
+# print(result)
+# print("boxes:")
+# for box in result[0].boxes:
+#   print(box)
+  
+  
+# predict 5
+# result = model.track('input_videos/input_video.mp4',conf=0.2, save=True)
+
